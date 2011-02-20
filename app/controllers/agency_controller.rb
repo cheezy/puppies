3b6045954b53ea7d93639a523ef3ea77aeba1ac4
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2011-02-20 10:25:26 -0500

    added pagination to main page

--- app/controllers/agency_controller.rb
+++ app/controllers/agency_controller.rb
@@ -1,7 +1,9 @@
+
 class AgencyController < ApplicationController
   skip_before_filter :authorize
+  
   def index
-    @puppies = Puppy.all
+    @puppies = Puppy.paginate :page => params[:page], :order => 'name', :per_page => 4
   end
 
 end
